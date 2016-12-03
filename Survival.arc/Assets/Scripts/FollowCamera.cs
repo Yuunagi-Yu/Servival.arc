67157b72706d8d7de41846fb3ed239750e362256
Yuunagi_Yu <yuunagi.yu@gmail.com>
2016-12-03 11:46:21 +0900

    I made a Game Field and how to operate Player's character

---
+++ Survival.arc/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+	public GameObject Target;
+	public Vector3 targetRelative;
+	private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (Target == null) {
+			return;
+		} else {
+			transform.position = Target.transform.position + targetRelative;
+			//transform.position = Vector3.Lerp (transform.position, newPosition, 0.7f);
+		}
+	}
+}
---
+++ Survival.arc/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float speed;
+	public GameObject laser;
+	private Vector3 moveDirection = Vector3.zero;
+	private Vector3 targetPoint = Vector3.zero;
+	private bool shot = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+		if (moveDirection.magnitude > 0.1f) {
+			transform.position += moveDirection * Time.deltaTime * speed;
+		}
+		if (shot) {
+			if (Input.GetMouseButtonDown (0) || Input.GetMouseButton(0)) {
+				RaycastHit hit;
+				Ray mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (mouseRay, out hit)) {
+					if (hit.transform.tag == "Field") {
+						targetPoint = new Vector3 (hit.point.x, transform.position.y, hit.point.z);
+						Instantiate (laser, transform.position, Quaternion.LookRotation (targetPoint - transform.position));
+						Instantiate (laser, transform.position, 
+							Quaternion.LookRotation (targetPoint - transform.position) * Quaternion.Euler(0, 15, 0));
+						Instantiate (laser, transform.position, 
+							Quaternion.LookRotation (targetPoint - transform.position) * Quaternion.Euler(0, -15, 0));
+						shot = false;
+						StartCoroutine (shotWait (0.2f));
+					}
+				}
+			}
+		}
+	}
+
+	IEnumerator shotWait(float waitTime){
+		yield return new WaitForSeconds (waitTime);
+		shot = true;
+	}
+}
